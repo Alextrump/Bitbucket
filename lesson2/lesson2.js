1152ed576c94203902ff28445ec3ba15f63316f0
Alextrump <57220890+Alextrump@users.noreply.github.com>
2019-12-05 15:57:46 +0500

    Update lesson2.js

--- lesson2/lesson2.js
+++ lesson2/lesson2.js
@@ -1,15 +1,14 @@
-'use strict'
 let data = [
-	{ score: 10, name: 'bob' , age: 18},
-  { score: 9, name: 'ivan' , age: 24},
+  { score: 10, name: 'Bob' , age: 18},
+  { score: 9, name: 'Ivan' , age: 24},
   { score: 8, name: 'Boris' , age: 14},
-  { score: 7, name: 'vasya' , age: 34},
-  { score: 6, name: 'ilya' , age: 15},
-  { score: 5, name: 'tod' , age: 54},
-  { score: 4, name: 'petr' , age: 32},
-  { score: 3, name: 'igor' , age: 44},
-  { score: 2, name: 'oleg' , age: 76},
-  { score: 1, name: 'morgan' , age: 37},
+  { score: 7, name: 'Vasya' , age: 34},
+  { score: 6, name: 'Ilya' , age: 15},
+  { score: 5, name: 'Tod' , age: 54},
+  { score: 4, name: 'Petr' , age: 32},
+  { score: 3, name: 'Igor' , age: 44},
+  { score: 2, name: 'Oleg' , age: 76},
+  { score: 1, name: 'Morgan' , age: 37},
 ];
 
 
